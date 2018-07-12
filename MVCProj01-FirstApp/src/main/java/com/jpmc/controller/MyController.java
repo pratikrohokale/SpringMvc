923fc801d6a44aba99c736472277101a153b0675
pratik <pratik@pratik-PC>
2018-07-12 22:34:28 +0530

    Created First Web App Using Spring MVC

---
+++ MVCProj01-FirstApp/src/main/java/com/jpmc/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.jpmc.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MyController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+
+		return new ModelAndView("welcome", "sysDate", new Date());
+	}
+
+}
