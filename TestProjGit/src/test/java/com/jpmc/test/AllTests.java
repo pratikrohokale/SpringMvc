d552113d24772b371ffbbac192476974f3dd091e
pratikrohokale <prohokale01@gmail.com>
2018-07-29 15:29:35 +0530

    Jira No: added build and test cases

---
+++ TestProjGit/src/test/java/com/jpmc/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.jpmc.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BankServiceTestCases.class })
+public class AllTests {
+
+}
